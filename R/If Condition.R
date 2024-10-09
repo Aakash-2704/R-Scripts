f9847ba1deca55ab12ed717fceea4f774cfe4d89
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-09 03:19:26 +0000

    Commit

---
+++ R/If Condition.R
@@ -0,0 +1,8 @@
+a <- 4
+b <- 3
+
+if (a>b){
+    print("a is greater")
+    } else {
+        ("b is greater")
+    }
---
+++ R/Strings.R
@@ -0,0 +1,12 @@
+
+name <- "My name is x"
+print(nchar(name))                # Here "nchar" is to check length of the string 
+
+
+y <- "eggs"
+print(grepl('g',y))              # grepl check given string is there in a variable
+
+
+c1 <- "Hello"
+c2 <- "World"
+print(paste(c1,c2))              # PASTE here concat two strings
