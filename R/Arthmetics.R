924741fa9505717f4859c7fec76be424bf6a738f
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-09 02:14:36 +0000

    R scripts

---
+++ R/Arthmetics.R
@@ -0,0 +1,14 @@
+a<- 4
+b<- 3
+
+print(a+b) #Addition
+print(a-b) #Subtraction
+print(a*b) #Multiplication
+print(a/b) #Division
+print(a>b) #Comparsion
+print(a||b) 
+
+print(class(a)) #class is like checking datatype
+c<-3i
+print(class(c))
+
