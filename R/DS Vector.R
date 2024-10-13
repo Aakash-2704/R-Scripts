140ef6d75fb7b967bf971d2f05b87c42a8be6651
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-13 17:04:03 +0000

    Adding DS Vectors

---
+++ R/DS Vector.R
@@ -0,0 +1,22 @@
+fruits <- c("banana","apple","kiwi")
+print(fruits)
+print(fruits[1])
+print(length(fruits))          # Printing length of a function
+
+
+number <- c(4,1,2,3)
+print(number)                  # Printing a number 
+print(sort(number))            # Sorting  
+
+
+numbers <- 1:10
+print(numbers)                       # Printing numbers from 1 to 10 
+
+number1 <- seq(from = 0, to = 100, by = 20)
+print(number1)
+
+repeat_each <- rep(c(1,2,3), each = 3)          # Printing each number for 3 times
+print(repeat_each)
+
+repeat_times <- rep(c(1,2,3),each = c(5,2,1))
+print(repeat_times)
