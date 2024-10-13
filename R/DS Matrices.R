9556e1b2dc9f5d3b20741d134b07e83b21f140c5
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-13 19:52:04 +0000

    Adding Matrices

--- R/DS Matrices.R
+++ R/DS Matrices.R
@@ -12,3 +12,10 @@ print(thismatrix[c(1,2),])
 thismatrix <- matrix(c("apple","banana","cherry","pineapple","grape","pineapple", "pear", "melon", "fig"), nrow = 3, ncol = 3)
 newmatrix <- rbind(thismatrix, c("strawberry", "blueberry", "raspberry"))
 print(newmatrix)
+
+Matrix1 <- matrix(c("apple", "banana", "cherry", "grape"), nrow = 2, ncol = 2)
+Matrix2 <- matrix(c("pineapple", "mango", "pineapple", "watermelon"), nrow = 2, ncol = 2)
+Matrix_Combined1 <- rbind(Matrix1, Matrix2)                                                      # Row Bind
+print(Matrix_Combined1)
+Matrix_Combined2 <- cbind(Matrix1, Matrix2)                                                      # Column Bind
+print(Matrix_Combined2)
