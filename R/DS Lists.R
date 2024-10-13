90ace259a6dd7143a601f735e8270c7b3e76b95e
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-13 17:11:44 +0000

    Adding Lists

---
+++ R/DS Lists.R
@@ -0,0 +1,13 @@
+thislist <- list("apple","banana","cherry")
+print(thislist)
+print(thislist[1])
+print(length(thislist))
+
+for (x in thislist){
+    print(x)
+}
+
+list1 <- list(1,2,3)
+list2 <- list(4,5,6)
+list3 <- c(list1,list2)
+print(list3)
