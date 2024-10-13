22d9a21c3b0f0e8e8298a8d7ef2bd8f74f00b275
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-13 17:18:47 +0000

    Adding Matrices

--- R/DS Matrices.R
+++ R/DS Matrices.R
@@ -5,3 +5,5 @@ print(matrix)
 thismatrix <- matrix(c("apple", "banana", "cherry", "biscuit"), nrow = 2, ncol = 2)
 print(thismatrix)
 
+thismatrix <- matrix(c("apple", "banana","carrot","cherry","grape","pineapple","pear","melon","fig"), nrow = 3, ncol = 3)
+print(thismatrix[c(1,2),])
