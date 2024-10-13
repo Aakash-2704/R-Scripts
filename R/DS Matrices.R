dfa2ae3be296479fcb176be24b109d9656950de6
Aakash-2704 <aakash.detne@yahoo.com>
2024-10-13 17:24:00 +0000

    Adding Matrices

--- R/DS Matrices.R
+++ R/DS Matrices.R
@@ -1,6 +1,7 @@
 
 matrix <- matrix(c(1,2,3,4,5,6), nrow=3, ncol=2)
 print(matrix)
+print(dim(matrix))
 
 thismatrix <- matrix(c("apple", "banana", "cherry", "biscuit"), nrow = 2, ncol = 2)
 print(thismatrix)
